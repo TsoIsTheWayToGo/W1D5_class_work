1e553b9f3dc43fcd7b9ca715e72796f23fc72b87
jhspark17 <jhspark17@gmail.com>
2019-01-11 17:01:20 -0800

    Add comment

--- polynodetree/lib/00_tree_node.rb
+++ polynodetree/lib/00_tree_node.rb
@@ -65,7 +65,12 @@ def bfs(target_value)
     end
     nil
 end
-#
+##
 end
 
+# git config --global user.name "Your Name"
+# git config --global user.email you@example.com
 
+# After doing this, you may fix the identity used for this commit with:
+
+# git commit --amend --reset-author
